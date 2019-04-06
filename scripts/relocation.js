9454ef3f99a1bf77983d07cff4d09cc350586bf2
KenyanBurnham <kenyan.burnham@ttu.edu>
2019-04-06 14:20:39 -0500

    Moving scripts and documenting head

---
+++ scripts/relocation.js
@@ -0,0 +1,15 @@
+function relocateTechnology(){
+    window.location.href('technology.html');
+}
+
+function relocateCommunity(){
+    window.location.href('community.html');
+}
+
+function relocateImpact(){
+    window.location.href('impact.html');
+}
+
+function relocateAbout(){
+    window.location.href('aboutUs.html');
+}
