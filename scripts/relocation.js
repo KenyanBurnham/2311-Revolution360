c5c62eefc2fa0561c1924c9fdfdfd1b2c790cef4
KenyanBurnham <kenyan.burnham@ttu.edu>
2019-04-08 16:16:33 -0500

    Fixing Navigation

--- scripts/relocation.js
+++ scripts/relocation.js
@@ -1,19 +1,27 @@
 function relocateTechnology(){
-    window.location.href('technology.html');
+    //window.location.href('technology.html');
+    window.location.assign("technology.html");
 }
 
 function relocateCommunity(){
-    window.location.href('community.html');
+    //window.location.href('community.html');
+    window.location.assign("community.html");
 }
+
 
 function relocateImpact(){
-    window.location.href('impact.html');
+    //window.location.href('impact.html');
+    window.location.assign("impact.html");
 }
+
 
 function relocateAbout(){
-    window.location.href('aboutUs.html');
+    //window.location.href('aboutUs.html');
+    window.location.assign("aboutUs.html");
 }
 
+
 function relocateHome(){
-    window.location.href('main.html');
+    //window.location.href('main.html');
+    window.location.assign("main.html");
 }
